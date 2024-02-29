c47f0bf7bda82f171f639021ec3a0bab588e203e
dell <dell@localhost>
2024-02-29 12:49:00 -0600

    111

--- src/studio5/Methods.java
+++ src/studio5/Methods.java
@@ -1,5 +1,7 @@
 package studio5;
 
+import static org.junit.Assert.assertEquals;
+
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Methods {
@@ -16,7 +18,7 @@ public class Methods {
 	public static double distanceBetween(double x1, double y1, double x2, double y2) {
 		double distance = 0;
 		// FIXME: Hint use Math methods (e.g. Math.sqrt) to compute the distance
-		
+		distance = Math.sqrt((x2-x1) * (x2-x1) + (y2-y1) * (y2-y1));
 		return distance;
 	}
 
@@ -34,17 +36,19 @@ public class Methods {
 		// TODO: Draw the remaining rings of the bull's eye
 		// Blue ring with 3.0/4.0 the radius
 		// suggested rgb values: 0, 109, 219
-
-		
+		StdDraw.setPenColor(StdDraw.BLUE);
+		StdDraw.filledCircle(x, y, radius*.75);
 
 		// Red ring with 1.0/2.0 the radius
 		// suggested rgb values: 146, 0, 0
-
+		StdDraw.setPenColor(StdDraw.RED);
+		StdDraw.filledCircle(x, y, radius*.5);
 		
 
 		// Yellow ring with 1.0/4.0 the radius
 		// suggested rgb values: 255, 255, 109
-
+		StdDraw.setPenColor(StdDraw.YELLOW);
+		StdDraw.filledCircle(x, y, radius*.25);
 		
 	}
 
@@ -58,14 +62,24 @@ public class Methods {
 	 * 
 	 * @return the String which results from substituting all of the target
 	 *         characters in the source String with the replacement String
+	 *         
 	 */
 	public static String substituteAll(String source, char target, String replacement) {
 		String result = "";
 		// TODO: Finish this method
-		
+		for (int i = 0; i < source.length(); i++) {
+			char character = source.charAt(i);
+			if (character == target){
+				result = result + replacement;
+				
+			}
+			else {
+				result = result + character;
+			}
+		 }
 		return result;
 	}
-
+	
 	/**
 	 * Compute the sum of elements in an array
 	 * 
@@ -75,7 +89,9 @@ public class Methods {
 	public static int arraySum(int[] values) {
 		int sum = 0;
 		// FIXME: Compute the sum of the values in an array
-		
+		for (int i = 0; i < values.length;i++) {
+			sum = sum + values[i];
+		}
 		return sum;
 	}
 
@@ -87,9 +103,12 @@ public class Methods {
 	 * @return and array of size that's filled with value
 	 */
 	public static int[] filledArray(int length, int value) {
-		int[] values = null; // FIXME: Create an array of the appropriate size
+	// FIXME: Create an array of the appropriate size
+		int[] values = new int[length];
 		// TODO: Finish this method
-
+		for(int i = 0; i < length; i++) {
+			values[i] = value;
+		}
 		
 
 		return values;
@@ -97,6 +116,12 @@ public class Methods {
 
 	// TODO: Create an arrayMean method which accepts an int array of values parameter.
 	// TODO: Create a JavaDoc comment for the arrayMean method.
-
+	public static double arrayMean(int[] numbers) {
+		
+		int newSum = arraySum(numbers);
+		double mean = newSum /  numbers.length;
+		return mean;
+		
+	}
 	
 }
--- test_src/studio5/tests/DistanceBetweenTest.java
+++ test_src/studio5/tests/DistanceBetweenTest.java
@@ -18,6 +18,7 @@ public class DistanceBetweenTest {
 	 * Test the distance between (1,0) and (1,0) is ~0.0
 	 */
 	@Test
+	
 	public void testIdenticalPoints() {
 		// The following is divided on three lines to make each part clearer.  See the comments on each
 		assertEquals(0.0, // The value we expect to get (to pass the test)
@@ -25,6 +26,7 @@ public class DistanceBetweenTest {
 				1e-5); // This uses double, which are imprecise, so
 						// the delta is used to decide how close is "close enough".
 						// In this case + or - 0.00001 is being used
+		//System.out.println(distanceBetween(1.0, 0.0, 1.0, 0.0));
 	}
 
 	/**
@@ -61,7 +63,12 @@ public class DistanceBetweenTest {
 	public void testPointsD() {
 		// TODO: FIXME!  This test has an error
 		// Test that the distance between (0,0) and (30,40) is 50.0 (order of parameters matters!)
-		assertEquals(50.0, Methods.distanceBetween(0.0, 30.0, 0.0, 40.0), 1e-5);
+		assertEquals(50.0, Methods.distanceBetween(0.0, 0.0, 30.0, 40.0), 1e-5);
+	}
+	public void testPointsE() {
+		// TODO: FIXME!  This test has an error
+		// Test that the distance between (0,0) and (30,40) is 50.0 (order of parameters matters!)
+		assertEquals(5, Methods.distanceBetween(8.0, 9.0, 5.0, 5.0), 1e-5);
 	}
 
 }
